712815ea9b845b199e67dfb7916375570fb4689b
Jorge Garcia Regueiro <jorge.garcia.regueiro@gmail.com>
2024-02-26 21:14:37 +0100

    subiendo req3

---
+++ src/main/java/entities/ArticuloDao.java
@@ -0,0 +1,32 @@
+package entities;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="inventario")
+public class ArticuloDao {
+
+	private List<Articulo> listaArticulos;
+	
+	public ArticuloDao() {
+		
+	}
+	
+	public ArticuloDao(List<Articulo> listaArticulos) {
+		this.listaArticulos = listaArticulos;
+	}
+	
+	@XmlElementWrapper(name = "articulos")
+	@XmlElement(name = "articulo")
+	public List<Articulo> getArticulos() {
+	    return listaArticulos;
+	}
+	
+	public void setArticulos(List<Articulo> articulos) {
+	    this.listaArticulos = articulos;
+	}
+
+}
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,49 @@
+package main;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import entities.Articulo;
+import entities.ArticuloDao;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JAXBContext context;
+		List<Articulo> listArticulos = new ArrayList<>();
+		listArticulos.add(new Articulo("articulo1","descripcion1",1,10));
+		listArticulos.add(new Articulo("articulo2","descripcion2",2,20));
+		listArticulos.add(new Articulo("articulo3","descripcion3",3,30));
+		
+		ArticuloDao adao = new ArticuloDao(listArticulos);
+		
+		try {
+			context = JAXBContext.newInstance(ArticuloDao.class);
+		} catch (JAXBException e) {
+			System.out.println("Error creando el contexto");
+			e.printStackTrace();			
+			return;
+		}
+		
+		Marshaller m;
+		
+		try {
+			
+			m = context.createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			
+			m.marshal(adao, new File("articulos.xml"));
+			System.out.println("Done");
+			
+		} catch (JAXBException e) {
+			System.out.println("Error convertiendo el objeto a formato XML");
+			e.printStackTrace();
+		}
+	}
+
+}
